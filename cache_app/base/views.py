dc8b5ae0407377e9bac9d1e64dd83c53dbb33168
limoo <limoodrice@gmail.com>
2024-01-28 11:56:42 +0300

    Added a views to consume and render data to a template from inventory api

--- cache_app/base/views.py
+++ cache_app/base/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+import requests
 
-# Create your views here.
+def products(request):
+    data = requests.get(f"http://127.0.0.1:8000/api/inventory")
+    products = data.json()
+    print(products)
+    context = {"products": products}
+    return render(request, "base/inventory.html",context)
+
+def product_by_category(request, category):
+    response = requests.get(f"http://127.0.0.1:8000/api/inventory/category/{category}/")
+    products = response.json()
+    context = {"products": products}
+    return render(request, "base/inventory.html",context)
+
+def product_by_subcategory(request, subcategory):
+    response = requests.get(f"http://127.0.0.1:8000/api/inventory/subcategory/{subcategory}/")
+    products = response.json()
+    context = {"products": products}
+    return render(request, "base/inventory.html",context)
