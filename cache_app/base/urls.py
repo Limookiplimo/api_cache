f3f995d2e9ef6ceaff0e3bad25ab29804127bbf3
limoo <limoodrice@gmail.com>
2024-01-28 13:32:37 +0300

    Registered urls for the base app views

--- cache_app/base/urls.py
+++ cache_app/base/urls.py
@@ -3,6 +3,5 @@ from . import views
 
 urlpatterns = [
     path("products/",views.products,name="product-list"),
-    path("products/category/<str:category>/", views.product_by_category, name="product-by-category"),
-    path("products/subcategory/<str:subcategory>/", views.product_by_subcategory, name="product-by-subcategory"),
+    path('products/search-products/', views.search_products, name='search-products'),
 ]
