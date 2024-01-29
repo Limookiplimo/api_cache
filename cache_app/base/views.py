5409de5e28d7aeb8261357d8656baa35a0436b4a
limoo <limoodrice@gmail.com>
2024-01-29 08:48:03 +0300

    Refactored code to include chaching mechanism for api responses

--- cache_app/base/views.py
+++ cache_app/base/views.py
@@ -1,26 +1,30 @@
 from django.shortcuts import render
-import requests
+from requests_cache.session import CachedSession
+from datetime import timedelta
+import os
 
 def products(request):
-    data = requests.get(f"http://127.0.0.1:8000/api/inventory")
+    session = CachedSession("../database/cache/inventory_cache", expire_after=timedelta(hours=1))
+    data = session.get(f"http://127.0.0.1:8000/api/inventory")
     products = data.json()
     context = {"products": products}
     return render(request, "base/inventory.html",context)
 
 def search_products(request):
     search_term = request.GET.get('search')
+    session = CachedSession("../database/cache/inventory_cache", expire_after=timedelta(hours=1))
     
     if not search_term:
-        data = requests.get("http://127.0.0.1:8000/api/inventory")
+        data = session.get("http://127.0.0.1:8000/api/inventory")
         products = data.json()
     else:
         products_endpoint = f"http://127.0.0.1:8000/api/inventory/product/{search_term}/"
         category_endpoint = f"http://127.0.0.1:8000/api/inventory/category/{search_term}/"
         subcategory_endpoint = f"http://127.0.0.1:8000/api/inventory/subcategory/{search_term}/"
         
-        product_data = requests.get(products_endpoint).json()
-        category_data = requests.get(category_endpoint).json()
-        subcategory_data = requests.get(subcategory_endpoint).json()
+        product_data = session.get(products_endpoint).json()
+        category_data = session.get(category_endpoint).json()
+        subcategory_data = session.get(subcategory_endpoint).json()
 
         if product_data:
             products = product_data
